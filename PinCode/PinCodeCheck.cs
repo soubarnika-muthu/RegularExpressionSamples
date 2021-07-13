bac7474161378b3543f9525b5972dc920b40171d
soubarnika-muthu <venkatachalapathymuthu@gmail.com>
2021-07-13 15:30:53 +0530

    [SOUBARNIKA]Validating PinCode Using Regex

---
+++ PinCode/PinCodeCheck.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace PinCode
+{
+    class PinCodeCheck
+    {
+        string pattern = "^[0-9]{6}$";
+        string[] inputs = { "400088","A400088","400088B"};
+        public void ValidatingPinCode()
+        {
+            Regex regex = new Regex(pattern);
+            Console.WriteLine("validating Pincodes");
+            IterateLoop(inputs, regex);
+        }
+        public static void IterateLoop(string[] arr, Regex regex)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                bool result = regex.IsMatch(arr[i]);
+                if (result == true)
+                {
+                    Console.WriteLine(arr[i] + "------->" + "Valid");
+                }
+                else
+                {
+                    Console.WriteLine(arr[i] + "------->" + "Invalid");
+                }
+            }
+        }
+    }
+}
---
+++ PinCode/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace PinCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Validating PinCode Using REGEX");
+            PinCodeCheck pinCode = new PinCodeCheck();
+            pinCode.ValidatingPinCode();
+        }
+    }
+}
